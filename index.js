bbd2782c0b25883b5c3a8ef7b58350c0332ca763
John Oziegbe <82390694+oseji@users.noreply.github.com>
2023-02-23 15:15:53 +0100

    added step 2
    updated the code so that the next button on the first step now also renders the nextstep into view

--- index.js
+++ index.js
@@ -1,18 +1,66 @@
 "use strict";
 
+const formSection = document.querySelector(".form-section");
+
+//STEP NUMBERS
+const stepOneNum = document.getElementById("step-1");
+const stepTwoNum = document.getElementById("step-2");
+const stepThreeNum = document.getElementById("step-3");
+const stepFourNum = document.getElementById("step-4");
+
+//STEP 1 INPUT
 const inputName = document.getElementById("name");
 const email = document.getElementById("email");
 const phoneNumber = document.getElementById("phone-number");
 
 const errorLabel = document.querySelectorAll(".error-label");
 
-const btnNext = document.querySelector(".form-btn");
+//NEXT STEP BTNS
+const btnNextOne = document.getElementById("next-one");
+const btnNextTwo = document.getElementById("next-two");
+const btnNextThree = document.getElementById("next-three");
+const btnNextFour = document.getElementById("next-four");
 
-// const nameError = document.querySelector(".name-error");
-// const emailError = document.querySelector(".email-error");
-// const phoneError = document.querySelector(".phone-error");
+//PREV STEP BTNS
+const btnPrevTwo = document.getElementById("prev-two");
+const btnPrevThree = document.getElementById("prev-three");
+const btnPrevFour = document.getElementById("prev-four");
 
-console.log(errorLabel);
+//STEP TEMPLATES
+const stepTwo = `<h1 class="form-heading">Select your plan</h1>
+<p class="form-sub-heading">
+  You have the option of monthly or yearly billing.
+</p>
+<div class="payment-card-grp">
+  <div class="card">
+    <img src="assets/images/icon-arcade.svg" class="card-img" />
+    <h1 class="card-heading">Arcade</h1>
+    <p class="card-price">$9/mo</p>
+  </div>
+  <div class="card">
+    <img src="assets/images/icon-advanced.svg" class="card-img" />
+    <h1 class="card-heading">Advanced</h1>
+    <p class="card-price">$12/mo</p>
+  </div>
+  <div class="card">
+    <img src="assets/images/icon-pro.svg" class="card-img" />
+    <h1 class="card-heading">Pro</h1>
+    <p class="card-price">$15/mo</p>
+  </div>
+</div>
+<div class="month-year-grp">
+  <p class="month-text">Month</p>
+  <div class="slider">
+    <span class="choice"></span>
+  </div>
+  <p class="year-text">Year</p>
+</div>
+<div class="btn-grp">
+  <button class="form-btn-prev" id="prev-two">Go Back</button>
+    <button class="form-btn-next" id="next-two">Next Step</button>
+</div>`;
+
+console.log(formSection);
 
 //CODE
 const inputValidation = function (inputField, inputNumber) {
@@ -23,7 +71,8 @@ const inputValidation = function (inputField, inputNumber) {
   }
 };
 
-btnNext.addEventListener("click", function (e) {
+//NEXT BTN FOR STEP 1
+btnNextOne.addEventListener("click", function (e) {
   e.preventDefault();
 
   console.log("clicked");
@@ -38,4 +87,19 @@ btnNext.addEventListener("click", function (e) {
   } else {
     errorLabel[1].classList.remove("hidden");
   }
+
+  //NEXT PAGE
+  if (
+    email.value.includes("@") &&
+    email.value.includes(".com") &&
+    inputName.value !== "" &&
+    phoneNumber.value !== ""
+  ) {
+    console.log("next page");
+    formSection.innerHTML = stepTwo;
+    stepOneNum.classList.remove("active-step");
+    stepTwoNum.classList.add("active-step");
+  }
 });
+
+//NEXT BTN FOR STEP 2
