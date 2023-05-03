3218a9257845915fb53bce9e953e3873c82c3976
oseji <jiade1233@gmail.com>
2023-05-03 16:23:38 +0100

    completed intro page design

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        stepsBGDesktop: 'url("/assets/images/bg-sidebar-desktop.svg")',
+        stepsBGMobile: 'url("/assets/images/bg-sidebar-mobile.svg")',
+      },
+      height: {
+        105: "426px",
+      },
+      colors: {
+        textColor: "#2E315A",
+      },
+    },
+  },
+  plugins: [],
+};
